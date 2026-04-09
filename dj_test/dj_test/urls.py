769fc1eb2f6a0f48580b14b0ebd654ca23c51341
William Starling <williamjonas@comcast.net>
2026-04-09 09:24:17 -0500

    added visits app to list of apps; created a local url file and added it to the main file.

--- dj_test/dj_test/urls.py
+++ dj_test/dj_test/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 
@@ -25,4 +25,5 @@ def hello(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello', hello),
+    path('', include('visits.urls')),
 ]
