7d5969faa5751d1ecb6180f3344f326b847076d8
William Starling <williamjonas@comcast.net>
2026-04-14 09:38:01 -0500

    updated the django version to 6.0.2 to avoid 4.x and python 3.12+ mismatch. changed the presentation of the vistis Module in the admin panel.

--- dj_test/visits/models.py
+++ dj_test/visits/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class Visits(models.Model):
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Visits: {self.count}"
+
+    class Meta:
+        verbose_name_plural = "Visits" #overrides the plural name of the model in the admin interface
