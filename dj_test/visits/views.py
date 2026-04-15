58f589892edb6453266efd79bc63c91b30c7defb
William Starling <williamjonas@comcast.net>
2026-04-15 15:11:12 -0500

    updated the view views to work again, so it creates a new view object each time is visited and them sums all views to the current website to get the visit count.

--- dj_test/visits/views.py
+++ dj_test/visits/views.py
@@ -1,17 +1,20 @@
 from .models import Visit
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index(request: HttpRequest):
     #gets the views object, and increments it by one each time the web page is visited, and then passes the value into the html template.
     #Also saves it to the database so it persists across server restarts.
-    v = Visit.objects.first()
-    v.count += 1
+    v = Visit(page="")
+    if request.user.is_authenticated: #if the user is logged in,
+        v.username = request.user.username #updated the username of the visit to be the users username isntead of anonymous.
     v.save()
 
+    visitors = Visit.objects.filter(page="") #get all the visits to this home page.
+
     context ={
-        "num_visits": v.count
+        "num_visits": visitors.count() #sum the number of visitors.
     }
     return render(request, "index.html", status=200, context=context)
