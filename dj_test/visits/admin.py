566a826adbc0ab839bdc4c294dc853b93a57392f
William Starling <williamjonas@comcast.net>
2026-04-16 09:52:17 -0500

    updated the admin panel to show more details and have more features for the Visits model.

--- dj_test/visits/admin.py
+++ dj_test/visits/admin.py
@@ -4,4 +4,7 @@ from .models import Visit
 # Register your models here.
 @admin.register(Visit)
 class VisitsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["__str__", "page", "username", "timestamp"]
+    list_filter = ["page", "username", "timestamp"] #the different fields we can specify filters for.
+    search_fields = ["page", "username"] #the different fields we can specify search for.
+    readonly_fields = ["timestamp"] #b/c it's read only, we have to add timestamp here for it to be displayed in the object.
--- dj_test/visits/models.py
+++ dj_test/visits/models.py
@@ -6,3 +6,6 @@ class Visit(models.Model):
     page = models.CharField(max_length=255) #the page that is being tracked, default is the home page
     username = models.CharField(max_length=255, default="anonymous") #the username of the visitor, default is anonymous
     timestamp = models.DateTimeField(auto_now_add=True) #the timestamp of the visit, automatically set to the current time when the object is created
+
+    def __str__(self):
+        return f"Visit {self.pk}"
