e2e002db35b7437ac57329e0668bce30a504e02f
William Starling <williamjonas@comcast.net>
2026-04-16 08:40:54 -0500

    Updated the code to keep track of the different paths that are visited, not just home. Updated HTML templates to display the information about visits to that page.

--- dj_test/visits/views.py
+++ dj_test/visits/views.py
@@ -4,17 +4,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request: HttpRequest):
+def index(request: HttpRequest, page=""):
     #gets the views object, and increments it by one each time the web page is visited, and then passes the value into the html template.
     #Also saves it to the database so it persists across server restarts.
-    v = Visit(page="")
+    v = Visit(page=page)
     if request.user.is_authenticated: #if the user is logged in,
         v.username = request.user.username #updated the username of the visit to be the users username isntead of anonymous.
     v.save()
 
-    visitors = Visit.objects.filter(page="") #get all the visits to this home page.
+    visitors = Visit.objects.filter(page=page) #get all the visits to this page.
 
     context ={
+        "page": page,
+        "visitors": visitors,
         "num_visits": visitors.count() #sum the number of visitors.
     }
     return render(request, "index.html", status=200, context=context)
