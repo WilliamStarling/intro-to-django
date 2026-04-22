9b42b48adaa361ba9d90cac7bbf77a40468dd70f
William Starling <williamjonas@comcast.net>
2026-04-22 15:56:23 -0500

    got the trivia site working by migrating the changes and creating a new virtual environment.

--- dj_test_structure2/config/settings.py
+++ dj_test_structure2/config/settings.py
@@ -121,7 +121,14 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
